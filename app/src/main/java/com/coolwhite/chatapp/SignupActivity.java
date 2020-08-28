e490041f4c98e1f3cc60ee7ce76c080972e65b97
rihan530 <renovatio530@gmail.com>
2020-08-28 22:46:43 +0900

    Firebase를 이용한 채팅 앱 클론 코딩 회원가입시 이미지 추가 및 업로드 2020.08.28

--- app/src/main/java/com/coolwhite/chatapp/SignupActivity.java
+++ app/src/main/java/com/coolwhite/chatapp/SignupActivity.java
@@ -1,11 +1,15 @@
 package com.coolwhite.chatapp;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,20 +17,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.coolwhite.chatapp.model.UserModel;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.UploadTask;
 
 public class SignupActivity extends AppCompatActivity {
 
+    private static final int PICK_FROM_ALBUM = 10;
     private EditText email;
     private EditText username;
     private EditText password;
     private Button signup;
 
     private  String splash_background;
+    private ImageView profile;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,6 +48,16 @@ public class SignupActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setStatusBarColor(Color.parseColor(splash_background));
         }
+
+        profile = (ImageView) findViewById(R.id.signupActivity_imageview_profile);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent, PICK_FROM_ALBUM);
+            }
+        });
 
         email = (EditText) findViewById(R.id.signupActivity_edittext_email);
         username = (EditText) findViewById(R.id.signupActivity_edittext_username);
@@ -58,16 +78,65 @@ public class SignupActivity extends AppCompatActivity {
                         .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
+                                final String uid = task.getResult().getUser().getUid();
 
-                                String uid = task.getResult().getUser().getUid();
-                                UserModel userModel = new UserModel();
-                                userModel.userName = username.getText().toString();
+                                FirebaseStorage.getInstance().getReference().child("userImages").child(uid).putFile(imageUri)
+                                        .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
 
-                                FirebaseDatabase.getInstance().getReference().child("users").child(uid).setValue(userModel);
+                                            @Override
+
+                                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                                                FirebaseStorage.getInstance().getReference().child("userImages").child(uid).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+
+                                                    @Override
+
+                                                    public void onSuccess(Uri uri) {
+
+                                                        String imageUrl = uri.toString();
+
+                                                        UserModel userModel = new UserModel();
+
+                                                        userModel.userName = username.getText().toString();
+
+                                                        userModel.profileImageUrl = imageUrl;
+
+                                                        FirebaseDatabase.getInstance().getReference().child("users").child(uid).setValue(userModel);
+
+                                                    }
+
+                                                });
+
+                                            }
+
+                                        });
+
+//                                FirebaseStorage.getInstance().getReference().child("userImages").child(uid).putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+//                                    @Override
+//                                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+//
+//                                        @SuppressWarnings("VisibleForTests")
+//                                        String imageUrl = task.getResult().getDownloadUrl().toString();
+//
+//                                        UserModel userModel = new UserModel();
+//                                        userModel.userName = username.getText().toString();
+//
+//                                        FirebaseDatabase.getInstance().getReference().child("users").child(uid).setValue(userModel);
+//                                    }
+//                                });
 
                             }
                         });
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_FROM_ALBUM && resultCode == RESULT_OK) {
+            profile.setImageURI(data.getData()); // 가운데 뷰를 변경
+            imageUri = data.getData();// 이미지 경로 원본
+        }
     }
 }
--- app/src/main/java/com/coolwhite/chatapp/model/UserModel.java
+++ app/src/main/java/com/coolwhite/chatapp/model/UserModel.java
@@ -3,4 +3,5 @@ package com.coolwhite.chatapp.model;
 public class UserModel {
 
     public String userName;
+    public String profileImageUrl;
 }
